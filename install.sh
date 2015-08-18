0d8f03c170ea30d8cc86cfed3d1732c2602f7979
Igor Zibarev <zibarev.i@gmail.com>
2015-08-18 17:59:48 +0600

    Finished install.sh reworking

--- install.sh
+++ install.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script installs all dotfiles by symlinking them to $HOME directory.
+################################################################################
 
-# define variables
-
-DOTFILES_DIR=`dirname $( readlink -f ${BASH_SOURCE[0]} )`
-DOTFILES=$DOTFILES_DIR/src/*
-BACKUP_DIR=~/.dotfiles~
+# define params
 DO_UPDATE=true
 VERBOSE_FLAG=""
-
-# allow bash to find .dotfiles
-if ! shopt -q dotglob ; then
-    shopt -s dotglob
-fi
+DRY_RUN=false
 
 # process params
-
-for i in "$@"
+for param in "$@"
 do
-case $i in
-    --update=no*)
+case $param in
+    -n|--update=no*)
         DO_UPDATE=false
     ;;
     -v|--verbose)
         VERBOSE_FLAG="-v"
+    ;;
+    -d|--dry-run)
+        DRY_RUN=true
     ;;
     *)
     ;;
 esac
 done
 
+# self-update if need
 if [ $DO_UPDATE = true ] ; then
     echo -n "Self updating... "
     git pull --quiet --recurse-submodules
@@ -37,38 +33,85 @@ if [ $DO_UPDATE = true ] ; then
     echo "done."
 fi
 
+# create backup dir if need
 if [ ! -d $BACKUP_DIR ] ; then
-    echo "Creating $BACKUP_DIR for backup of any existing dotfiles in ~"
-    mkdir -p $BACKUP_DIR
+    mkdir -p $VERBOSE_FLAG $BACKUP_DIR
 fi
 
-echo "Changing to the $DOTFILES_DIR directory"
-cd $DOTFILES_DIR
+# determine path variables
+DOTFILES_ROOT=`dirname $( readlink -f ${BASH_SOURCE[0]} )`
+DOTFILES_SRC="src"
+BACKUP_DIR=~/.dotfiles~
 
-# move any existing dotfiles (or symlinks) in homedir to BACKUP_DIR, then create symlinks
-
-# @todo make a mapper file to manage directories
-
-echo "Installation started."
-for FILE in $DOTFILES; do
+# All regular files in $DOTFILES_SRC directory
+# should be symlinked directly to $HOME
+echo "Installing files"
+for FILE in `find $DOTFILES_ROOT/$DOTFILES_SRC -maxdepth 1 -mindepth 1 -type f`
+do
     FILENAME=`basename $FILE`
+    FILEPATH=`readlink -f $FILE`
 
-    # dirs, prefixed with double underline should be symlinked resursively
-    if [[ ${FILENAME} == __* ]] ; then
-        FILENAME=`echo $FILENAME | sed s/__//`
-        cp -sRf $VERBOSE_FLAG ${FILE}/* $HOME/${FILENAME}
+    # if symlink - delete it; elseif exists - backup
+    if [ -h $HOME/$FILENAME ] ; then
+        if [ $DRY_RUN = true ] ; then
+            echo "Removing $HOME/$FILENAME"
+        else
+            rm $VERBOSE_FLAG $HOME/$FILENAME
+        fi
+    elif [ -e $HOME/$FILENAME ] ; then
+        if [ $DRY_RUN = true ] ; then
+            echo "Moving $HOME/$FILENAME to $BACKUP_DIR"
+        else
+            mv -f $VERBOSE_FLAG $HOME/$FILENAME $BACKUP_DIR
+        fi
+    fi
 
+    if [ $DRY_RUN = true ] ; then
+        echo "Symlinking $HOME/$FILENAME -> $FILEPATH"
+    else
+        ln -sf $VERBOSE_FLAG $FILEPATH $HOME/$FILENAME
+    fi
+done
+
+# define variables needed for further processing
+MAPPING_DELIMETER="->"
+
+# All directories in $DOTFILES_SRC directory
+# should be symlinked to $HOME relatively,
+# according to the rules defined in mapper file.
+echo "Installing directories"
+while read line || [[ -n "$line" ]]; do
+    DIR_SRC=`echo ${line%$MAPPING_DELIMETER*}`
+    DIR_DEST=`echo ${line#*$MAPPING_DELIMETER}`
+
+    if [ ! -d $DOTFILES_ROOT/$DOTFILES_SRC/$DIR_SRC ] ; then
+        echo "Error: directory from mapping $DOTFILES_ROOT/$DOTFILES_SRC/$DIR_SRC does not exist"
         continue
     fi
 
-    if [ -h $HOME/$FILENAME ]; then
-        rm $VERBOSE_FLAG $HOME/$FILENAME
+    # if symlink - delete it; elseif exists - backup
+    if [ -h $HOME/$DIR_DEST ] ; then
+        if [ $DRY_RUN = true ] ; then
+            echo "Removing $HOME/$DIR_DEST"
+        else
+            rm $VERBOSE_FLAG $HOME/$DIR_DEST
+        fi
+    elif [ -e $HOME/$DIR_DEST ] ; then
+        if [ $DRY_RUN = true ] ; then
+            echo "Moving $HOME/$DIR_DEST to $BACKUP_DIR/$DIR_DEST"
+        else
+            [ ! -e `dirname $BACKUP_DIR/$DIR_DEST` ] && mkdir -p `dirname $BACKUP_DIR/$DIR_DEST`
+            mv -f $VERBOSE_FLAG $HOME/$DIR_DEST $BACKUP_DIR/$DIR_DEST
+        fi
     fi
 
-    if [ -e ~/$FILENAME ]; then
-       mv -f $VERBOSE_FLAG ~/$FILENAME $BACKUP_DIR
+    if [ $DRY_RUN = true ] ; then
+        echo "Symlinking $HOME/$DIR_DEST -> $DOTFILES_ROOT/$DOTFILES_SRC/$DIR_SRC"
+    else
+        ln -sfn $VERBOSE_FLAG $DOTFILES_ROOT/$DOTFILES_SRC/$DIR_SRC $HOME/$DIR_DEST
     fi
+done < $DOTFILES_ROOT/mapping
 
-    ln -sf $VERBOSE_FLAG $FILE ~/$FILENAME
-done
 echo "Installation finished."
+
+exit 0
