6eb87a6c34aded714900bd6bd7bc4bb66bb21bb6
Igor Zibarev <zibarev.i@gmail.com>
2016-04-08 18:15:15 +0600

    merge alias

--- src/.bashrc.d/aliases.sh
+++ src/.bashrc.d/aliases.sh
@@ -22,6 +22,26 @@ alias cb='xclip -selection clipboard'
 
 alias diffs="cdiff -s --ignore-all-space"
 
+# TODO move outside of aliases
+merge() {
+    local empty_file="empty"
+    local merge_file="merge"
+
+    if [ -z "$1" ] || [ -z "$2" ]; then
+        echo >&2 "Not enough arguments"
+        return 1
+    fi
+
+    if [ -e "./${merge_file}" ] || [ -e "./${empty_file}" ]; then
+        echo >&2 "File with name \"${merge_file}\" or \"${empty_file}\" exists."
+        return 1
+    fi
+
+    touch "${empty_file}"
+    git merge-file -p "$1" "${empty_file}" "$2" > ${merge_file}
+    rm "${empty_file}"
+}
+
 # >:]
 alias suka="sudo"
 
@@ -40,7 +60,7 @@ alias mansearch='apropos'
 # X
 alias reload-xresources="xrdb -merge ~/.Xresources"
 
-if [ "$(lsb_release)" = "Ubuntu" ]; then
+if [[ "$(lsb_release -i)" =~ "Ubuntu" ]]; then
     alias ap='sudo aptitude'
     alias apti='sudo aptitude install'
     alias aptu='sudo aptitude update'
