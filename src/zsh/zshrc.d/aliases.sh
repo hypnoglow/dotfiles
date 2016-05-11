6b95c69d77c49306d5d7dc23411e490871a75805
Igor Zibarev <zibarev.i@gmail.com>
2016-05-11 19:44:43 +0600

    [zsh] Aliases

--- src/zsh/zshrc.d/aliases.sh
+++ src/zsh/zshrc.d/aliases.sh
@@ -6,6 +6,7 @@
 # Default arguments for basic commands
 #
 alias cp="cp -i"
+alias free="free -m"
 
 #
 # Shortcuts
@@ -28,3 +29,33 @@ alias atom-install="apm install --production --packages-file ~/.atom/packages.li
 #
 alias gst="git status"
 alias gcb="git checkout -b"
+
+#
+# Simple functions
+#
+
+ex () {
+    if [ -z "$1" ] ; then
+        echo >&2 'File not specified'
+        return 1
+    fi
+
+    if [ ! -f "$1" ] ; then
+        echo "'$1' is not a valid file"
+    fi
+
+    case $1 in
+        *.tar.bz2)   tar xjf $1   ;;
+        *.tar.gz)    tar xzf $1   ;;
+        *.bz2)       bunzip2 $1   ;;
+        *.rar)       unrar x $1   ;;
+        *.gz)        gunzip $1    ;;
+        *.tar)       tar xf $1    ;;
+        *.tbz2)      tar xjf $1   ;;
+        *.tgz)       tar xzf $1   ;;
+        *.zip)       unzip $1     ;;
+        *.Z)         uncompress $1;;
+        *.7z)        7z x $1      ;;
+        *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+}
