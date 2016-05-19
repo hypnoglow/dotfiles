13b6e5f5de276dce92c5721bc2d662580a88a83e
Igor Zibarev <zibarev.i@gmail.com>
2016-05-19 20:49:45 +0600

    [zsh] bindkey replace command word

--- src/zsh/zshrc.d/keybindings.sh
+++ src/zsh/zshrc.d/keybindings.sh
@@ -2,10 +2,27 @@
 # of ZIM framework.
 
 zle-custom-backward-kill-subword() {
-    local WORDCHARS=${WORDCHARS//[\/]}
+    local WORDCHARS=${WORDCHARS//[\/-]}
     zle backward-kill-word
 }
 zle -N zle-custom-backward-kill-subword
 
+zle-custom-replace-first-word-of-last-command() {
+     zle up-history
+     zle beginning-of-line
+     zle delete-word
+}
+zle -N zle-custom-replace-first-word-of-last-command
+
+zle-custom-replace-last-word-of-last-command() {
+    zle up-history
+    zle end-of-line
+    zle backward-delete-word
+}
+zle -N zle-custom-replace-last-word-of-last-command
+
 bindkey "^[^?" zle-custom-backward-kill-subword
 bindkey "^H" backward-kill-word
+
+bindkey "^[" zle-custom-replace-first-word-of-last-command
+bindkey "^]" zle-custom-replace-last-word-of-last-command
