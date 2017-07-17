ddaf7b71afb75ad2046167725507c2e6af8cc964
Igor Zibarev <zibarev.i@gmail.com>
2017-07-17 21:40:15 +0300

    [zsh] Add gcloud completion

--- zsh/zshrc.d/40-completions.sh
+++ zsh/zshrc.d/40-completions.sh
@@ -4,3 +4,8 @@ aws_completion="/usr/local/bin/aws_zsh_completer.sh"
 if [ -e "$aws_completion" ]; then
     source "$aws_completion"
 fi
+
+gcloud_completion="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+if [ -e "$gcloud_completion" ]; then
+    source "$gcloud_completion"
+fi
