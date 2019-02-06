85c8e5d8a420656e2459583a95a4d7023466acb2
Igor Zibarev <zibarev.i@gmail.com>
2019-02-06 18:35:29 +0300

    [zsh] export KUBECONFIG

--- zsh/zshrc.d/01-exports.sh
+++ zsh/zshrc.d/01-exports.sh
@@ -1,1 +1,8 @@
 export BAT_THEME="OneHalfLight" # https://github.com/sharkdp/bat
+
+export KUBECONFIG="$HOME/.kube/config"
+for conf in $HOME/.kube/conf.d/* ; do
+    [ ! -f "$conf" ] && continue
+    export KUBECONFIG=$KUBECONFIG:$conf
+done
+
