e0ebd8530cec9e8b02cab0c9d9d05cb8c0e7ace6
Igor Zibarev <zibarev.i@gmail.com>
2019-04-24 19:32:48 +0300

    [zsh] move kubeps1 to macos-specific

--- zsh/zshrc.d/00-includes.sh
+++ zsh/zshrc.d/00-includes.sh
@@ -4,11 +4,5 @@
 
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
-# https://github.com/jonmosco/kube-ps1
-# TODO: move to macos-specific
-if [ -f /usr/local/opt/kube-ps1/share/kube-ps1.sh ] ; then
-    . /usr/local/opt/kube-ps1/share/kube-ps1.sh
-fi
-
 # https://github.com/direnv/direnv
 [ -x "$(which direnv 2>/dev/null)" ] && eval "$(direnv hook zsh)"
